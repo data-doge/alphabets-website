9db1db1ff86d52e5452ea29f619cab6771dfd11a
data-doge <eugene@enspiral.com>
2017-01-06 00:55:57 -0500

    add horizon and shitty sun

--- src/engine/view/index.js
+++ src/engine/view/index.js
@@ -2,8 +2,17 @@ const $ = require('jquery')
 
 class View {
 
+  constructor () {
+    this.renderSun()
+  }
+
   closeLoadingScreen () {
     $('#loading-screen').hide()
+  }
+
+  renderSun () {
+    this.$sun = $('<div id="sun"></div>')
+    $('body').append(this.$sun)
   }
 
 }
