58865081911ab2c97b1c4975d448d07dd5c3f64e
data-doge <eugene@enspiral.com>
2017-01-08 16:14:40 -0500

    shitty sun day passing

--- src/engine/view/index.js
+++ src/engine/view/index.js
@@ -13,6 +13,19 @@ class View {
   renderSun () {
     this.$sun = $('<div id="sun"></div>')
     $('body').append(this.$sun)
+    const centerX = $(window).width() / 2 - this.$sun.width() / 2
+    const centerY = $(window).height() / 2 - this.$sun.height() / 2
+    const a = 500
+    const b = 300
+    let theta = 0
+
+    setInterval(t => {
+      this.$sun.offset({
+        top: centerY + b * Math.sin(theta),
+        left: centerX + a * Math.cos(theta)
+      })
+      theta -= Math.PI / 180 / 5
+    }, 40)
   }
 
 }
