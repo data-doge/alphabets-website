cccafac502570f1852be70085924c5ae69add59e
Eugene Lynch <eugene@enspiral.com>
2017-04-16 14:55:16 -0400

    fuck ie

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+const $ = require('jquery')
+const detectie = require('detectie')
 const Engine = require('./engine')
 
-var engine = new Engine()
-engine.bindEventListeners()
-engine.start()
+if (detectie()) {
+  $('#fuck-ie').css('display', 'flex')
+} else {
+  const engine = new Engine()
+  engine.bindEventListeners()
+  engine.start()
+}
