aeab998413ebac33a248d40694877edb7529dd6e
Eugene Lynch <eugene@enspiral.com>
2017-04-16 02:24:16 -0400

    update audio

---
+++ src/engine/audio-interface/audio-files.js
@@ -0,0 +1,14 @@
+const audioFiles = [
+  '3-7.wav',
+  '4-10-3.wav',
+  '4-10.wav',
+  'day14edit.wav',
+  'day31.wav',
+  'fourthtapething72.wav',
+  'new-thing-10-30-edit.wav',
+  'shakethedust.wav',
+  'tapething721.wav',
+  'untitled006.wav'
+]
+
+module.exports = audioFiles
